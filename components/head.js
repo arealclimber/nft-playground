bd7930154533ed11278a5bd1acf1612dd0b4a5df
arealclimber <arealclimber@users.noreply.github.com>
2022-05-18 00:35:50 +0800

    change Head to one of the Layout component format

--- components/head.js
+++ components/head.js
@@ -1,40 +1,35 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-export default function PageHead({ children }) {
+export default function PageHead() {
 	return (
-		<>
-			<Head>
-				<div>
-					<title>NFT Playground</title>
+		<Head>
+			<div>
+				<title>NFT Playground</title>
 
-					<nav className="border-b p-6">
-						<Link href="/">
-							<p className="text-4xl font-bold hover:text-gray-500 cursor-pointer">NFT Playground</p>
+				<nav className="border-b p-6">
+					<Link href="/">
+						<p className="text-4xl font-bold hover:text-gray-500 cursor-pointer">NFT Playground</p>
+					</Link>
+					<div className="flex mt-4">
+						<Link href="/market">
+							<a className=" mr-6 font-bold hover:text-blue-400 text-blue-500">Market</a>
 						</Link>
-						<div className="flex mt-4">
-							<Link href="/market">
-								<a className="inline-block ui-monospace mr-6 font-bold hover:text-blue-400 text-blue-500">
-									Market
-								</a>
-							</Link>
-							<Link href="/loans">
-								<a className="mr-6 font-bold hover:text-blue-400 text-blue-500">NFT Loan</a>
-							</Link>
-							<Link href="/community">
-								<a className="mr-6 font-bold hover:text-blue-400 text-blue-500">Community</a>
-							</Link>
-							<Link href="/create-item">
-								<a className="mr-6 font-bold hover:text-blue-400 text-blue-500">Create NFT</a>
-							</Link>
-							<Link href="/my-assets">
-								<a className="mr-6 font-bold hover:text-blue-400 text-blue-500">My NFT</a>
-							</Link>
-						</div>
-					</nav>
-				</div>
-			</Head>
-			<main>{children}</main>
-		</>
+						<Link href="/loans">
+							<a className="mr-6 font-bold hover:text-blue-400 text-blue-500">NFT Loan</a>
+						</Link>
+						<Link href="/community">
+							<a className="mr-6 font-bold hover:text-blue-400 text-blue-500">Community</a>
+						</Link>
+						<Link href="/create-item">
+							<a className="mr-6 font-bold hover:text-blue-400 text-blue-500">Create NFT</a>
+						</Link>
+						<Link href="/my-assets">
+							<a className="mr-6 font-bold hover:text-blue-400 text-blue-500">My NFT</a>
+						</Link>
+					</div>
+				</nav>
+			</div>
+		</Head>
 	)
 }
