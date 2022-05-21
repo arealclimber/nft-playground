1bdcd28de64d1f02638929e7fad76eb06bb5abea
Lumii <arealclimber@users.noreply.github.com>
2022-05-21 08:12:14 +0000

    code about community

--- contracts/community.sol
+++ contracts/community.sol
@@ -6,31 +6,103 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+import "hardhat/console.sol";
+
 
 // Accumulate the ERC20 token for exchaging the TREE NFT!
-contract Feeds is Ownable, ReentrancyGuard {
+contract Feeds is Ownable, ReentrancyGuard, ERC721URIStorage, ERC721Enumerable {
+    using Strings for uint256;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NFT", "NFT") {
+    }
+
+    // Required overrides from parent contracts
+    function _burn(uint256 tokenId) internal virtual override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+        // return string(abi.encodePacked(super.tokenURI(tokenId), ".json"));
+        return super.tokenURI(tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    
+    
     uint memberCount;
 
     struct Snippet {
         uint likeCount;
+        uint flagCount;
         address author;
         string content;
         bool restricted;
-
     }
 
+    // mapping(address => (uint => Snippet[])) authorToSnippets;
+    mapping(address => Snippet[]) authorToSnippets;
+    
+    uint dailyLimit;
+    uint lowerBound;
+
+    function setLowerBound(uint _num) onlyOwner nonReentrant returns (uint) {
+        lowerBound = _num;
+        return lowerBound;
+    }
+
+    function setDailyLimit(uint _num) onlyOwner nonReentrant returns (uint) {
+        dailyLimit = _num;
+        return dailyLimit;
+    }
+
+    
+    
     // Tree NFT of ERC20 !!
 
     mapping(address => uint) membership;
 
     Snippet[] public snippets;
 
-    modifier ifMemeber(address checkAddress) {
+    modifier isMemeber(address checkAddress) {
         require(membership[checkAddress] > 0);
         _;
+    }
+
+    modifier canClaim(address checkAddress) {
+        require(authorToSnippets[checkAddress].length > lowerBound);
+        _;
+    }
+
+    function publish(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+
+        return newItemId;
     }
 
 
     
 
 }
+
+
+
+
+
--- test/marketTest.t.js
+++ test/marketTest.t.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
-describe('NFT Market', function () {
-	it('Should create and execute market sales', async function () {
+describe('NFT Market', function() {
+	it('Should create and execute market sales', async function() {
 		const Market = await ethers.getContractFactory('testNFTMarket');
 		const market = await Market.deploy();
 		await market.deployed();
@@ -13,44 +13,43 @@ describe('NFT Market', function () {
 		await nft.deployed();
 		const nftContractAddress = nft.address;
 
-        let commision = await market.getCommision();
-        commision = commision.toString();
+		let commision = await market.getCommision();
+		commision = commision.toString();
 
 		const auctionPrice = ethers.utils.parseUnits('999', 'ether');
 
 		await nft.createToken('https://www.mytokenlocation.com');
 		await nft.createToken('https://www.mytokenlocation.com');
 
-        // `0` and `1` are `tokenId`
-        // being on sale then getting the `itemId` respectively
-        await market.addItemToMarket(0, auctionPrice, nft);
-        await market.addItemToMarket(1, auctionPrice, nft);
+		// `0` and `1` are `tokenId`
+		// being on sale then getting the `itemId` respectively
+		await market.addItemToMarket(0, auctionPrice, nft);
+		await market.addItemToMarket(1, auctionPrice, nft);
 
 		// Local testing environment: using the test accounts
 		// Have the buyer and the seller be different person
 		const [_, buyerAddress] = await ethers.getSigners();
 
+		await market.connect(buyerAddress).buyItem(0, { value: auctionPrice });
 
-        await market.connect(buyerAddress).buyItem(0, {value: auctionPrice});
-
-        // TODO: Test if the marketplace receive the commision
-        // TODO: Test if the seller receive the commision
+		// TODO: Test if the marketplace receive the commision
+		// TODO: Test if the seller receive the commision
 
 		let items = await market.fetchMarketItems();
 
 		// Set the items to be the result of this mapping
 		items = await Promise.all(
-			items.map(async (i) => {
+			items.map(async i => {
 				const tokenUri = await nft.tokenURI(i.tokenId);
 				let item = {
 					price: i.price.toString(),
 					tokenId: i.tokenId.toString(),
-                    itemId: i.itemId.toString(),
+					itemId: i.itemId.toString(),
 					seller: i.seller,
-                    contract: i.nftContract,
-                    isSold: i.isSold,
-                    isOnSale: i.isOnSale,
-					tokenUri,
+					contract: i.nftContract,
+					isSold: i.isSold,
+					isOnSale: i.isOnSale,
+					tokenUri
 				};
 				return item;
 			})
