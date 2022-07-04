2318e4f740674453699a7aeba8c04094fab68fbc
arealclimber <luffulgence@gmail.com>
2022-07-04 18:13:59 +0800

    verify contracts with Hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,18 @@
-require('@nomiclabs/hardhat-waffle')
-require('dotenv').config()
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-etherscan');
+// const dotenv = require('dotenv');
+// dotenv.config({ path: '/.env' });
+require('dotenv').config();
+
+const {
+	INFURA_MUMBAI_URL,
+	ACCOUNT_PRIVATE_KEY,
+	ALCHEMY_RINKEBY_URL,
+	MUMBAI_API_KEY,
+} = process.env;
+// console.log(require('dotenv').config());
+// console.log('process.env.ACCOUNT_PRIVATE_KEY', process.env.ACCOUNT_PRIVATE_KEY);
 
 module.exports = {
 	solidity: {
@@ -17,14 +30,18 @@ module.exports = {
 			chainId: 1337,
 		},
 		mumbai: {
-			url: process.env.INFURA_MUMBAI_URL,
-			accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+			url: INFURA_MUMBAI_URL,
+			accounts: [ACCOUNT_PRIVATE_KEY],
 		},
 		rinkeby: {
-			url: process.env.ALCHEMY_RINKEBY_URL,
-			accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+			url: ALCHEMY_RINKEBY_URL,
+			accounts: [ACCOUNT_PRIVATE_KEY],
 		},
-
 		// mainnet: {},
 	},
-}
+	etherscan: {
+		apiKey: {
+			polygonMumbai: MUMBAI_API_KEY,
+		},
+	},
+};
--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ const nextConfig = {
 	env: {
 		INFURA_IPFS_PROJECT_ID: process.env.INFURA_IPFS_PROJECT_ID,
 		INFURA_IPFS_PROJECT_SECRET: process.env.INFURA_IPFS_PROJECT_SECRET,
-		ACCOUNT_PRIVATE_KEY: process.env.ACCOUNT_PRIVATE_KEY,
+		// ACCOUNT_PRIVATE_KEY: process.env.ACCOUNT_PRIVATE_KEY,
 		ALCHEMY_MUMBAI_URL: process.env.ALCHEMY_MUMBAI_URL,
 		ALCHEMY_MUMBAI_ID: process.env.ALCHEMY_MUMBAI_ID,
 		INFURA_MUMBAI_URL: process.env.INFURA_MUMBAI_URL,
 		ALCHEMY_RINKEBY_URL: process.env.ALCHEMY_RINKEBY_URL,
 	},
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
